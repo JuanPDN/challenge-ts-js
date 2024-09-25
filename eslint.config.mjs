4ac0544d695f441c701824ce81c049b5bba751f6
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-25 17:44:46 -0500

    chore: configure ESLint to work with Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import jest from "eslint-plugin-jest"
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: [globals.node, globals.jest] }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  
+  jest.configs["flat/recommended"]
 ];
