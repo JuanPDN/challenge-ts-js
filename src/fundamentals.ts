328e3553ee7f4308656ae3e0863898232622ad72
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-25 16:45:56 -0500

    feat: add new exercise in fundamentals

--- src/fundamentals.ts
+++ src/fundamentals.ts
@@ -1,15 +1,31 @@
-/* Sentence Smash
- Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
- You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word.Be careful,
+/* 
+Sentence Smash
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
+You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word.Be careful,
 there shouldn't be a space at the beginning or the end of the sentence!
 
- Example:
- ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'*/
+Example:
 
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'*/
 
 const smash = (words: Array<string>): string => {
-
+    // Your code here
     return ""
 }
 
-export { smash }
+/*
+How good are you really?
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return true if you're better, else false!
+
+Note:
+Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+*/
+
+const betterThanAverage = (classPoints: number[], yourPoints: number): boolean => {
+    // Your code here
+    return true
+}
+
+export { smash, betterThanAverage }
--- src/tests/fundamentals.test.ts
+++ src/tests/fundamentals.test.ts
@@ -1,18 +1,38 @@
-import { smash } from '../fundamentals'
+import { smash, betterThanAverage } from '../fundamentals'
 
 
 describe("Smash words", () => {
-    test('One word example should return the word.', () => {
+    it("One word example should return the word.", () => {
         expect(smash(["hello"])).toMatch("hello")
     })
 
-    test("Should return empty string for empty array.", () => {
+    it("Should return empty string for empty array.", () => {
         expect(smash([])).toMatch("")
     })
 
-    test("Multiple words should be separated by spaces.", () => {
+    it("Multiple words should be separated by spaces.", () => {
         expect(smash(["hello", "world"])).toMatch("hello world")
         expect(smash(["hello", "amazing", "world"])).toMatch("hello amazing world")
         expect(smash(["this", "is", "a", "long", "sentence"])).toMatch("this is a long sentence")
     })
+})
+
+describe("Better Than Average", () => {
+    it("betterThanAverage([2, 3], 5) should return True", () => {
+        expect(betterThanAverage([2, 3], 5)).toBe(true)
+    })
+    it("betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75) should return True", () => {
+        expect(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)).toBe(true)
+    })
+    it("betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9) should return False", function () {
+        expect(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)).toBe(false);
+    });
+
+    it("betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50) should return False", function () {
+        expect(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50)).toBe(false);
+    });
+
+    it("betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21) should return False", function () {
+        expect(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21)).toBe(false);
+    });
 })
