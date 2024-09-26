9bb1cfc3372e957e0ec6624200502eb4252bfcfa
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-09-26 13:58:51 -0500

    feat: add new exercise in fundamentals

--- src/tests/fundamentals.test.ts
+++ src/tests/fundamentals.test.ts
@@ -1,4 +1,4 @@
-import { smash, betterThanAverage } from '../fundamentals'
+import { smash, betterThanAverage, rentalCarCost } from '../fundamentals'
 
 
 describe("Smash words", () => {
@@ -36,3 +36,24 @@ describe("Better Than Average", () => {
         expect(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21)).toBe(false);
     });
 })
+
+describe("Regular Tests:", function () {
+    it("Should work for under 3 days:", function () {
+        expect(rentalCarCost(1)).toBe(40);
+        expect(rentalCarCost(2)).toBe(80);
+    });
+
+    it("Should work for under 7 days:", function () {
+        expect(rentalCarCost(3)).toBe(100);
+        expect(rentalCarCost(4)).toBe(140);
+        expect(rentalCarCost(5)).toBe(180);
+        expect(rentalCarCost(6)).toBe(220);
+    });
+
+    it("Should work for 7 or more days:", function () {
+        expect(rentalCarCost(7)).toBe(230);
+        expect(rentalCarCost(8)).toBe(270);
+        expect(rentalCarCost(9)).toBe(310);
+        expect(rentalCarCost(10)).toBe(350);
+    });
+});
